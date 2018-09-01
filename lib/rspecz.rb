788335d190288e7482390592ea4c5c532136b27d
Zenn <app.zenn@gmail.com>
2018-09-01 16:57:13 +0900

    fix require rspec/core before start

--- lib/rspecz.rb
+++ lib/rspecz.rb
@@ -1,11 +1,9 @@
-if Module.const_defined?('RSpec::Core')
-  require 'rspecz/version'
-  require 'rspecz/contexts'
-  require 'rspecz/subjects'
-  require 'rspecz/lets'
-  require 'rspecz/with'
-  require 'rspecz/behave'
-  require 'rspecz/aliases/behave'
-  require 'rspecz/aliases/make'
-end
-
+require 'rspec/core'
+require 'rspecz/version'
+require 'rspecz/contexts'
+require 'rspecz/subjects'
+require 'rspecz/lets'
+require 'rspecz/with'
+require 'rspecz/behave'
+require 'rspecz/aliases/behave'
+require 'rspecz/aliases/make'
