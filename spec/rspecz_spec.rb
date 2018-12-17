5fd14984848ff3e8868d6efe4d7af4546dfad935
shota-yamashita <s.yamashita@bbank.jp>
2018-12-18 08:38:28 +0900

    remove unnecessary spec

--- spec/rspecz_spec.rb
+++ spec/rspecz_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RSpecZ do
   it "has a version number" do
     expect(RSpecZ::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
