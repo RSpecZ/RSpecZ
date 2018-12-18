6cbc29f9266eee7e6e55fe1080fd2b7b9e4281cb
shota-yamashita <s.yamashita@bbank.jp>
2018-12-18 09:11:02 +0900

    add_spec[wip]

--- spec/rspecz_spec.rb
+++ spec/rspecz_spec.rb
@@ -1,5 +1,15 @@
 RSpec.describe RSpecZ do
-  it "has a version number" do
-    expect(RSpecZ::VERSION).not_to be nil
+  it { expect(RSpecZ::VERSION).to eq "0.1.1" }
+
+  set_valid(:bool, true) do
+    it { expect(bool).to be_truthy }
+  end
+
+  set_invalid(:bool, false) do
+    it { expect(bool).to be_falsy }
+  end
+
+  set_invalids(:num, 2, 4, 6, 12) do
+    it { expect(num).not_to be_odd }
   end
 end
